8fa1974eea40da20d50e9d3a6f928ca1ab070741
jennieisa <jennieisakssoon97@gmail.com>
2022-03-11 20:51:26 +0100

    Created a function that fetches the selected characters

--- main.js
+++ main.js
@@ -34,3 +34,66 @@ class Characters {
 
 
 }
+
+//Hämtar från DOM:en
+const compareCharactersBtn = document.querySelector(".compareCharactersBtn");
+
+let character1 = "";
+
+let character2 = "";
+
+let characters = [];
+
+//Extra 
+function ifCharacterIsAlreadyChoosen() {
+
+}
+
+async function fetchData(url) {
+
+    const response = await fetch(url)
+
+    const data = await response.json();
+
+    return data;
+
+}
+
+async function fetchCaracters(name1, name2) {
+
+    let array = [];
+
+    let searchValue1 = name1.replace("_", " ");
+
+    let searchValue2 = name2.replace("_", " ");
+
+    character1 = await fetchData(`https://swapi.dev/api/people/?search=${searchValue1}`);
+
+    character2 =  await fetchData(`https://swapi.dev/api/people/?search=${searchValue2}`);
+
+    array.push(character1.results[0]);
+
+    array.push(character2.results[0]);
+
+    return array;
+
+}
+
+function drawCharacter() {
+
+}
+
+compareCharactersBtn.addEventListener("click", async (e) => {
+
+    e.preventDefault();
+
+    const selectedCharacter1 = document.querySelector("#character1").value;
+
+    const selectedCharacter2 = document.querySelector("#character2").value;
+
+    characters = await fetchCaracters(selectedCharacter1, selectedCharacter2); 
+
+    console.log(characters)
+
+})
+
