3cf4c1538cd637200b4f773d844428f24f0c164f
jennieisa <jennieisakssoon97@gmail.com>
2022-03-11 22:50:45 +0100

    Created function that compares height

--- main.js
+++ main.js
@@ -44,8 +44,31 @@ class Characters {
 
     }
 
-    compareHeight() {
+    compareHeight(character) {
 
+        let result = "";
+
+        let sum = "";
+
+        if(this.height < character.height) {
+
+            sum = parseInt(this.height) / parseInt(character.height);
+            
+            result = `<p>${character.name} är ${Math.round(sum)} % längre än mig. </p>`;
+            
+        } else if (this.height > character.height) {
+
+            sum = parseInt(character.height) / parseInt(this.height);
+            
+            result = `<p>${character.name} är ${Math.round(sum)}% kortare än mig. </p>`;
+
+        } else {
+
+            result = `<p>${character.name} är lika lång som mig!</p>`;
+
+        }
+
+        return result;
     }
 
     compareHairColor() {
@@ -139,6 +162,7 @@ function drawCharacter(character1, character2) {
         <button class="character1QuestionBtn" id="character1HeightBtn">Hur lång är ${character2.name}?</button>
         <button class="character1QuestionBtn" id="character1HairColorBtn">Vilken hårfärg har ${character2.name}?</button>
         <button class="character1QuestionBtn" id="character1GenderBtn">Vilket kön har ${character2.name}?</button>
+        <article class="character1Answ"></article>
     `;
 
     const character2Content = `
@@ -148,6 +172,7 @@ function drawCharacter(character1, character2) {
         <button class="character2QuestionBtn" id="character2HeightBtn">Hur lång är ${character1.name}?</button>
         <button class="character2QuestionBtn" id="character2HairColorBtn">Vilken hårfärg har ${character1.name}?</button>
         <button class="character2QuestionBtn" id="character2GenderBtn">Vilket kön har ${character1.name}?</button>
+        <article class="character2Answ"></article>
     `;
 
     character1Wrapper.innerHTML = character1Content;
@@ -182,27 +207,57 @@ compareCharactersBtn.addEventListener("click", async (e) => {
 
     drawCharacter(character1, character2);
 
+    const character1Answ = document.querySelector(".character1Answ");
+
+    const character2Answ = document.querySelector(".character2Answ");
+
     const character1WeightBtn = document.querySelector("#character1WeightBtn");
+
+    const character2WeightBtn = document.querySelector("#character2WeightBtn");
+
+    const character1HeightBtn = document.querySelector("#character1HeightBtn");
+
+    const character2HeightBtn = document.querySelector("#character2HeightBtn");
+
+    const character1HairColorBtn = document.querySelector("#character1HairColorBtn");
+
+    const character2HairColorBtn = document.querySelector("#character2HairColorBtn");
+
+    const character1GenderBtn = document.querySelector("#character1GenderBtn");
+
+    const character2GenderBtn = document.querySelector("#character2GenderBtn");
 
     character1WeightBtn.addEventListener("click", () => {
 
-        const result = character1.compareMass(character2);
+        let result = character1.compareMass(character2);
         
-        character1Wrapper.innerHTML += result;
+        character1Answ.innerHTML = result;
 
     })
-
-    const character2WeightBtn = document.querySelector("#character2WeightBtn")
 
     character2WeightBtn.addEventListener("click", () => {
 
-        const result = character2.compareMass(character1);
+        let result = character2.compareMass(character1);
 
-        character2Wrapper.innerHTML += result;
+        character2Answ.innerHTML = result;
 
     })
 
-    
+    character1HeightBtn.addEventListener("click", () => {
+
+        let result = character1.compareHeight(character2);
+
+        character1Answ.innerHTML = result;
+
+    })
+
+    character2HeightBtn.addEventListener("click", () => {
+
+        let result = character2.compareHeight(character1);
+
+        character2Answ.innerHTML = result;
+
+    })
 
 })
 
