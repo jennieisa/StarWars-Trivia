f890631c0a8b70f8a1e4ba307fba7d700aab9a82
jennieisa <jennieisakssoon97@gmail.com>
2022-03-11 19:53:44 +0100

    Created class characters

---
+++ main.js
@@ -0,0 +1,36 @@
+class Characters {
+
+    constructor(name, gender, height, mass, hairColor, pictureUrl) {
+
+        this.name = name;
+
+        this.gender = gender;
+
+        this.height = height;
+
+        this.mass = mass;
+
+        this.hairColor = hairColor;
+
+        this.pictureUrl = pictureUrl;
+
+    }
+
+    compareMass() {
+
+    }
+
+    compareHeight() {
+
+    }
+
+    compareHairColor() {
+
+    }
+
+    compareGender() {
+        
+    }
+
+
+}
